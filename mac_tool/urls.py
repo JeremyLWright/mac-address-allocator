11660dd1768cbd2bcf2c1253f420ce999075b2e5
Jeremy <Jeremy@BlueShift.ph.cox.net>
2012-12-01 17:34:47 -0700

    Routed simple page through to the MAC tool

--- mac_tool/urls.py
+++ mac_tool/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -5,6 +6,5 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^mac/', include(mac_tool.site.urls)),
-    url(r'^admin/', include(admin.site.urls)),
-)
+        url(r'^$', 'mac_tool.views.index'),
+        )
--- mac_tool/views.py
+++ mac_tool/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+"""
+Copyright Jeremy Wright (c) 2012
+Creative Commons Attribution-ShareAlike 3.0 Unported License
+"""
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate
+from django.utils import simplejson
+
+def index(request):
+    return HttpResponse("Hello")
+
